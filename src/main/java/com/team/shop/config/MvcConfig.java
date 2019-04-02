db93ccfa79ff13a0bd5c31fbefd66e200cde566d
blackprm <1144569608@qq.com>
2019-04-02 18:15:59 +0800

    druid数据配置完成,druid测试成功 -- 问题: 配置完websocket时,springboot不能进行单元测试 -- 解决: 在单元测试类中加上@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) 注解

--- src/main/java/com/team/shop/config/MvcConfig.java
+++ src/main/java/com/team/shop/config/MvcConfig.java
@@ -1,7 +1,11 @@
 package com.team.shop.config;
 
 
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import com.team.shop.interceptor.AuthenticationInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
@@ -19,5 +23,27 @@ public class MvcConfig implements WebMvcConfigurer {
     @Bean
     public AuthenticationInterceptor authenticationInterceptor(){
         return new AuthenticationInterceptor();
+    }
+
+
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");
+        reg.addInitParameter("loginUsername", "root");
+        reg.addInitParameter("loginPassword", "root");
+        return reg;
+    }
+
+
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.addInitParameter("profileEnable", "true");
+        return filterRegistrationBean;
     }
 }
